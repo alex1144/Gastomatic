0f14c9cde5e72718e66f5dcae0c9f769c093cb64
AlexBecerra1144 <abecerra@occ.com.mx>
2016-09-07 16:05:49 -0500

    Se hace commit de modelos nuevamente

---
+++ GastoMatic/Controllers/CuentaGastoController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GastoMatic.Models;
+
+namespace GastoMatic.Controllers
+{
+    public class CuentaGastoController : Controller
+    {
+        //
+        // GET: /CuentaGasto/
+
+        public ActionResult Index()
+        {
+            CuentaGastos cg = new CuentaGastos();
+            cg.listCuentaGastos();
+            return View();
+        }
+
+        //
+        // GET: /CuentaGasto/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /CuentaGasto/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /CuentaGasto/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /CuentaGasto/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CuentaGasto/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CuentaGasto/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CuentaGasto/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- GastoMatic/Models/CuentaGastos.cs
+++ GastoMatic/Models/CuentaGastos.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Collections;
 
 namespace GastoMatic.Models
 {
-    public class CuentaGastos
+    public class CuentaGastos : ActiveRecord
     {
         public int IdCuentaGastos { get; set; }
         public DateTime FechaCreacion { get; set; }
@@ -13,11 +15,66 @@ namespace GastoMatic.Models
         public DateTime FechaFinal { get; set; }
         public string NumeroAcreedor { get; set; }
         public string Descripcion { get; set; }
-
+        
         public bool crearCuentaGasto() {
             bool resultado = false;
 
             return resultado;
+        }
+
+        public bool modificarCuentaGasto()
+        {
+            bool resultado = false;
+
+            return resultado;
+        }
+
+        public CuentaGastos verCuentaGastos() {
+            return this;
+        }
+
+        public ArrayList listCuentaGastos() { 
+            ArrayList lista = new ArrayList();
+            string datosConexion = this.cadenaConexion;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(datosConexion))
+                {
+                    //Paso 2 - Abrir la conexión
+                    con.Open();
+
+                    // Paso 3 - Crear un nuevo comando
+
+                    string textoCmd = "SELECT * FROM CuentaGastos";
+
+                    SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(String.Format(" {0},{1}",
+                            reader[0], reader[1]));
+                        }
+                    }
+                    catch (SqlException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    reader.Close();
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            return lista;
         }
     }
 }
