9df2c5b14b5b7cb57fabb3061b89b6aa0740b42e
bzavalaf <bzavala@occ.com.mx>
2016-09-08 16:48:38 -0500

    Proyecto de pruebas

---
+++ GastoMaticTest.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GastoMatic;
+using GastoMatic.Controllers;
+
+namespace GastoMaticTest.Test.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void HControllerIndex()
+        {
+            HomeController controller= new HomeController();
+            ViewResult result = (ViewResult)controller.Index(); //as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
